a1e0d3b7089e93fc333a8257b9630dbe2c41ec04
Dennis <d.vdMeulen98@gmail.com>
2018-10-15 13:15:12 -0700

    Started work on docker python files

--- docker/count.py
+++ docker/count.py
@@ -1,5 +1,25 @@
 #!/bin/python3
 
+import sqlite3
+from os import listdir
+
 # TODO: Tel in de documenten in de container hoevaak de variabele die doorgegeven is door CoCo voorkomt
 
 # TODO: Voeg de opgehaalde informatie toe aan de index in de sqlite database
+conn = sqlite3.connect('Wordlist.db')
+c = conn.cursor()
+
+term = "hoi" # TODO: Zoekterm aanleveren
+
+# Controleer of de zoekterm al in de database staat
+c.execute("""SELECT quantity FROM wordlist WHERE term = ?"""), term
+indb = c.fetchall()
+if len(indb) is 0:
+    # TODO Woorden tellen in de textbestanden in de
+    listdir("/files/file*.txt")
+
+    file=open("/files/")
+    c.execute("""INSERT INTO wordlist(term, quantity, docnr) VALUES (?,?,?)""")
+
+conn.commit()
+conn.close()
--- docker/reduce-count.py
+++ docker/reduce-count.py
@@ -1,5 +1,9 @@
 #!/bin/python3
 
+
+sumcontainer = 2 # TODO: som van voorgaande containers
+nextcontainer = 102 # TODO: huidig containernummer + 1 (misschien array met alle beschikbare nummers)
+
 # TODO: haal uit de database van de huidige container op hoe vaak de variabele bestaat
 
 # TODO: voeg toe aan de variabele (Lijst of int)?????????????????????????
