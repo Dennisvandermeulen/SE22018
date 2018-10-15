947c22f0c71ff6430773083a5b467462f017d883
Dennis <d.vdMeulen98@gmail.com>
2018-10-15 15:14:39 -0700

    cleaned up TODO's

--- docker/count.py
+++ docker/count.py
@@ -1,16 +1,12 @@
 #!/bin/python3
 
 import sqlite3
-import glob
 from os import listdir
 
-# TODO: Tel in de documenten in de container hoevaak de variabele die doorgegeven is door CoCo voorkomt
-
-# TODO: Voeg de opgehaalde informatie toe aan de index in de sqlite database
 conn = sqlite3.connect('Wordlist.db')
 c = conn.cursor()
 
-# TODO: Zoekterm aanleveren
+# TODO: Zoekterm aanleveren vanuit CoCo
 searchterm = "hallo"
 
 # Controleer of de zoekterm al in de database staat
